aad9df7ad452eb9d2792b2d4c134c45d16b8ec73
Mikecamdo <mcdoherty@smu.edu>
2023-04-17 18:33:35 -0500

    Function headers and test file
    Created the function headers for the three main functions. Also created a tester.py file that can be used to invoke and test these functions

--- cs5322s23.py
+++ cs5322s23.py
@@ -1,1 +1,10 @@
-print('Hello World!')
+def WSD_Test_Rubbish(list):
+    print('Inside Rubbish function')
+
+
+def WSD_Test_Yarn(list):
+    print('Inside Yarn function')
+
+
+def WSD_Test_Tissue(list):
+    print('Inside Tissue function')
---
+++ tester.py
@@ -0,0 +1,4 @@
+from cs5322s23 import *
+
+temp = [ ]
+WSD_Test_Rubbish(temp)
