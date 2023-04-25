8e10a432986123374e8f8388add8f49b83f3521d
Mikecamdo <mcdoherty@smu.edu>
2023-04-24 23:28:25 -0500

    Basic logic
    Added the basic logic to compare a given sentence to the two definitions of each word. Also added the custom sentences other students provided (to be used as testing data later)

--- cs5322s23.py
+++ cs5322s23.py
@@ -1,43 +1,141 @@
 from nltk.wsd import lesk
 from nltk.tokenize import word_tokenize
 from nltk.corpus import wordnet as wn
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer, PorterStemmer
 
 def WSD_Test_Rubbish(list): # List is a list of strings, with each string being a sentence containing the word "rubbish"
-    options = wn.synsets('rubbish')
-    options.pop()
     answer = [ ]
-    for sentence in list:
-        a1 = lesk(word_tokenize(sentence), 'rubbish', 'n', options)
-        if a1.definition() == options[0].definition():
+    definition1 = 'rubbish, trash, scrap (worthless material that is to be disposed of)'
+    definition2 = 'folderol, rubbish, tripe, trumpery, trash, wish-wash, applesauce, codswallop (nonsensical talk or writing)'
+
+    for sentence in list: #iterates through every sentence
+        words = word_tokenize(sentence)
+        lemmatizer = WordNetLemmatizer()
+        stopWords = set(stopwords.words('english'))
+        filtered_words = [ ]
+        for word in words: #filters out stop words
+            if word not in stopWords:
+                word = lemmatizer.lemmatize(word) #lemmatizes words
+                filtered_words.append(word)
+
+        def1Similarity = 0 #the sentence's similarity to the first definition
+        def2Similarity = 0 #the sentence's similarity to the second definition
+
+        def1Words = word_tokenize(definition1)
+        def2Words = word_tokenize(definition2)
+        
+        for word in filtered_words:
+            for otherWord in def1Words:
+                try:
+                    word1 = wn.synset(word + '.n.01')
+                    word2 = wn.synset(otherWord  + '.n.01')
+                    def1Similarity += word1.wup_similarity(word2) #calculates how similar the definitions of the two words are
+                except:
+                    def1Similarity += 0
+
+            for otherWord in def2Words:
+                try:
+                    word1 = wn.synset(word + '.n.01')
+                    word2 = wn.synset(otherWord  + '.n.01')
+                    def2Similarity += word1.wup_similarity(word2) #calculates how similar the definitions of the two words are
+                except:
+                    def2Similarity += 0
+
+        if def1Similarity > def2Similarity:
             answer.append(1)
-        elif a1.definition() == options[1].definition():
+        else:
             answer.append(2)
 
     return answer
 
 def WSD_Test_Yarn(list):
-    options = wn.synsets('yarn')
-    options.pop()
     answer = [ ]
-    for sentence in list:
-        a1 = lesk(word_tokenize(sentence), 'yarn', 'n', options)
-        if a1.definition() == options[0].definition():
+    definition1 = 'narration, recital, yarn (the act of giving an account describing incidents or a course of events)'
+    definition2 = 'thread, yarn (a fine cord of twisted fibers (of cotton or silk or wool or nylon etc.) used in sewing and weaving)'
+
+    for sentence in list: #iterates through every sentence
+        words = word_tokenize(sentence)
+        lemmatizer = WordNetLemmatizer()
+        stopWords = set(stopwords.words('english'))
+        filtered_words = [ ]
+        for word in words: #filters out stop words
+            if word not in stopWords:
+                word = lemmatizer.lemmatize(word) #lemmatizes words
+                filtered_words.append(word)
+
+        def1Similarity = 0 #the sentence's similarity to the first definition
+        def2Similarity = 0 #the sentence's similarity to the second definition
+
+        def1Words = word_tokenize(definition1)
+        def2Words = word_tokenize(definition2)
+        
+        for word in filtered_words:
+            for otherWord in def1Words:
+                try:
+                    word1 = wn.synset(word + '.n.01')
+                    word2 = wn.synset(otherWord  + '.n.01')
+                    def1Similarity += word1.wup_similarity(word2) #calculates how similar the definitions of the two words are
+                except:
+                    def1Similarity += 0
+
+            for otherWord in def2Words:
+                try:
+                    word1 = wn.synset(word + '.n.01')
+                    word2 = wn.synset(otherWord  + '.n.01')
+                    def2Similarity += word1.wup_similarity(word2) #calculates how similar the definitions of the two words are
+                except:
+                    def2Similarity += 0
+
+        if def1Similarity > def2Similarity:
             answer.append(1)
-        elif a1.definition() == options[1].definition():
+        else:
             answer.append(2)
 
     return answer
 
 
 def WSD_Test_Tissue(list):
-    options = wn.synsets('tissue')
-    options.pop()
     answer = [ ]
-    for sentence in list:
-        a1 = lesk(word_tokenize(sentence), 'tissue', 'n', options)
-        if a1.definition() == options[0].definition():
+    definition1 = 'tissue (part of an organism consisting of an aggregate of cells having a similar structure and function)'
+    definition2 = 'tissue, tissue paper (a soft thin (usually translucent) paper)'
+
+    for sentence in list: #iterates through every sentence
+        words = word_tokenize(sentence)
+        lemmatizer = WordNetLemmatizer()
+        stopWords = set(stopwords.words('english'))
+        filtered_words = [ ]
+        for word in words: #filters out stop words
+            if word not in stopWords:
+                word = lemmatizer.lemmatize(word) #lemmatizes words
+                filtered_words.append(word)
+
+        def1Similarity = 0 #the sentence's similarity to the first definition
+        def2Similarity = 0 #the sentence's similarity to the second definition
+
+        def1Words = word_tokenize(definition1)
+        def2Words = word_tokenize(definition2)
+        
+        for word in filtered_words:
+            for otherWord in def1Words:
+                try:
+                    word1 = wn.synset(word + '.n.01')
+                    word2 = wn.synset(otherWord  + '.n.01')
+                    def1Similarity += word1.wup_similarity(word2) #calculates how similar the definitions of the two words are
+                except:
+                    def1Similarity += 0
+
+            for otherWord in def2Words:
+                try:
+                    word1 = wn.synset(word + '.n.01')
+                    word2 = wn.synset(otherWord  + '.n.01')
+                    def2Similarity += word1.wup_similarity(word2) #calculates how similar the definitions of the two words are
+                except:
+                    def2Similarity += 0
+
+        if def1Similarity > def2Similarity:
             answer.append(1)
-        elif a1.definition() == options[1].definition():
+        else:
             answer.append(2)
 
     return answer
--- tester.py
+++ tester.py
@@ -102,46 +102,24 @@ for i in range(len(WSD_Solution3)):
 print('Accuracy:', correct / len(WSD_Solution3))
 print(len(yarn_sentences), len(yarn_sentences_tags), len(WSD_Solution3))
 
-
-
-#rubbish_sentences = [ 
-#    'There was a thick film of dust on every exposed surface; rubbish and the carcass of some small animal had liecn swept carelessly into a corner.',
-#    "There's nothing under here except a large amount of mineral salts and other rubbish that's no use to us.",
-#    'I dropped the satin rubbish on the floor, because it could only hamper me.',
-#    'Like a man possessed he hurried around the room, tumbling rubbish and papers into piles and igniting them with his pocket lighter.',
-#    'Rubbish and offal filled the corners.',
-#    "Wendy wouldn't be sucked in by a load of specious rubbish spouted by a cracked demagogue hungry for martyrdom.",
-#    "\"Oh, let's Stop talking rubbish,\" she cried.",
-#    'Intelligent design should be viewed as a ground-clearing operation that gets rid of the intellectual rubbish that for generations has kept Christianity from receiving serious consideration.',
-#    'Nonsensical talk or rubbish'
-# ]
-
 '''
+from pprint import pprint
+pprint(WSD_Solution)
+print('----------------------')
+pprint(WSD_Solution2)
+print('----------------------')
+pprint(WSD_Solution3)
 
-WSD_Test_Rubbish(rubbish_sentences)
+from nltk.corpus import wordnet as wn
 
-print('-----------------------------------------------------------------')
+#word1 = wn.synset('rubbish.n.01')
+#word2 = wn.synset('trash.n.01')
 
-testing2 = [
-    'As the weeks passed, he put on weight, removed the fatty tissue from his cheekbones, thickened his cheeks enough to remove the gauntness, and restored his complexion to a healthier hue.',
-    'But she had flicked scar tissue and I answered almost sharply, "I am not employee of Warden."',
-    'I strained with every bit of muscle tissue to no useful end.',
-    'He wiped his nose and reached for a fresh tissue.',
-    'Japanese tissue may be made from one of three plants, the kōzo plant (Broussonetia papyrifera, paper mulberry tree), the mitsumata (Edgeworthia chrysantha) shrub and the gampi tree (Diplomorpha sikokiana).',
-    'He took a clean issue tissue tunic from the wall dispenser.'
-]
+for syn in wn.synsets('rubbish'):
+    for i in syn.lemmas():
+        print(i.name())
 
-WSD_Test_Tissue(testing2)
-
-print('-----------------------------------------------------------------')
-
-testing3 = [
-    "The yarn is no longer novel -- too many other writers have since taken off from Gallun's inspiration -- but it is just as fine to me as it always was.",
-    'I have just finished reading a rather lengthy yarn with several rather far-fetched theories in one field, two or three political premises stirred up with them, and then a bunch of characters, none clearly definable, trying to juggle the whole mess.',
-    'Several thousand carbon fibers are twisted together to form a yarn, which may be used by itself or woven into a fabric.',
-    'Warp knitting is defined as a loop-forming process in which the yarn is fed into the knitting zone, parallel to the fabric selvage.'
-]
-
-WSD_Test_Yarn(testing3)
-
+#print(word1)
+#print(word2)
+#print(word1.wup_similarity(word2))
 '''
